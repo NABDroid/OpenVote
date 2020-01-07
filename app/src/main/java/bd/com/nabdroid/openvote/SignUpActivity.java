27f2975ab71aa750a7a6580e4f00cd09656b32c4
NABDroid <nabdroid313@gmail.com>
2020-01-07 16:52:23 +0600

    Updated java code

--- app/src/main/java/bd/com/nabdroid/openvote/SignUpActivity.java
+++ app/src/main/java/bd/com/nabdroid/openvote/SignUpActivity.java
@@ -1,15 +1,26 @@
 package bd.com.nabdroid.openvote;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
     private EditText userNameET, mobileNumberET, emailET, passwordET, confirmPasswordET;
     private Button registerBTN;
+    String userName, mobileNumber, email, password, confirmPassword;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +30,17 @@ public class SignUpActivity extends AppCompatActivity {
         registerBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                userName = userNameET.getText().toString().trim();
+                mobileNumber = mobileNumberET.getText().toString().trim();
+                email = emailET.getText().toString().trim();
+                password = passwordET.getText().toString().trim();
+                confirmPassword = confirmPasswordET.getText().toString().trim();
+                if (confirmPassword.equals(password)) {
+                    createUser(email, password);
+                }
+                else {
+                    Toast.makeText(SignUpActivity.this, "Password didn't mached", Toast.LENGTH_SHORT).show();
+                }
 
             }
         });
@@ -26,11 +48,22 @@ public class SignUpActivity extends AppCompatActivity {
 
     }
 
+    private void createUser(String email, String password) {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(SignUpActivity.this, HomeActivity.class));
+                        }
+                        else {
+                            String error = task.getException().toString().trim();
+                            Toast.makeText(SignUpActivity.this, "Message: "+error+"", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
 
-
-
-
-
+    }
 
 
     private void init() {
@@ -40,5 +73,6 @@ public class SignUpActivity extends AppCompatActivity {
         passwordET = findViewById(R.id.passwordETSU);
         confirmPasswordET = findViewById(R.id.confirmPasswordETSU);
         registerBTN = findViewById(R.id.registerBTNSU);
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 }
---
+++ app/src/main/java/bd/com/nabdroid/openvote/UserProfile.java
@@ -0,0 +1,47 @@
+package bd.com.nabdroid.openvote;
+
+public class UserProfile {
+    private String userName, mobileNumber, email, password;
+
+    public UserProfile(String userName, String mobileNumber, String email, String password) {
+        this.userName = userName;
+        this.mobileNumber = mobileNumber;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UserProfile() {
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}

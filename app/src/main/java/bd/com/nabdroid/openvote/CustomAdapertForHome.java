2ce8d9935db570530725ad8c2257be96b1ba17ce
NABDroid <nabdroid313@gmail.com>
2020-01-18 01:29:36 +0600

    update 18.01.20

--- app/src/main/java/bd/com/nabdroid/openvote/CustomAdapertForHome.java
+++ app/src/main/java/bd/com/nabdroid/openvote/CustomAdapertForHome.java
@@ -3,20 +3,25 @@ package bd.com.nabdroid.openvote;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
 public class CustomAdapertForHome extends RecyclerView.Adapter<CustomAdapertForHome.ViewHolder> {
 
+    final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
     private ArrayList<Vote> votes;
-
     public CustomAdapertForHome(ArrayList<Vote> votes) {
         this.votes = votes;
     }
@@ -36,42 +41,82 @@ public class CustomAdapertForHome extends RecyclerView.Adapter<CustomAdapertForH
     public void onBindViewHolder(@NonNull final CustomAdapertForHome.ViewHolder holder, int position) {
 
 
-        String code, topic, creatorName, comment, agreedVote, disagreedVote;
-        final int endTime;
+        final String code, topic, creatorId, comment;
+        final int endTime, yesVote, noVote;
 
         //collecting form DB
         final Vote vote = votes.get(position);
-        creatorName = vote.getCreatorId();
+
         code = vote.getVoteCode();
         topic = vote.getTopic();
+        creatorId = vote.getCreatorId();
         endTime = vote.getEndTime();
+        yesVote = vote.getYesVote();
+        noVote = vote.getNoVote();
+
+
+
 
         //showing data to homeActivityUI
-        holder.userNameTV.setText(creatorName);
+        holder.userNameTV.setText(creatorId);
         holder.topicTV.setText(topic);
         holder.titleTV.setText(code);
         holder.lifetimeTV.setText(Integer.toString(endTime));
 
         //sending data to db
-
-        holder.submitIV.setOnClickListener(new View.OnClickListener() {
+        holder.submitBTN.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                //get selected option from radioGroup
                 int voteId = holder.radioGroup.getCheckedRadioButtonId();
+
+                //checking selected option
                 if (voteId == R.id.agreeRBHAUI){
-                    int previousAgreedVote = vote.getYesVote();
-                    vote.setYesVote(previousAgreedVote+1);
+                    int newVote = yesVote+1;
+
+                    //upgrade yes vote to database
+                    DatabaseReference currentNodeRef = databaseReference.child("Votes").child(creatorId).child("yesVote");
+                    currentNodeRef.setValue(newVote).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            String agreedVote = Integer.toString(vote.getYesVote());
+                            String disagreedVote = Integer.toString(vote.getNoVote());
+                            holder.voteCountTV.setText(agreedVote+" people agreed with you and "+disagreedVote+" people disagreed with you!");
+                            notifyDataSetChanged();
+                            holder.radioGroup.setVisibility(View.INVISIBLE);
+                            holder.commentET.setVisibility(View.INVISIBLE);
+                            holder.submitBTN.setVisibility(View.INVISIBLE);
+
+
+                        }
+                    });
+
+
+
                 }
                 else if(voteId == R.id.disagreeRBHAUI){
-                    int previousDisgreedVote = vote.getNoVote();
-                    vote.setNoVote(previousDisgreedVote+1);
+                    int newVote = noVote+1;
+
+                    //upgrade no vote to database
+                    DatabaseReference currentNodeRef = databaseReference.child("Votes").child(creatorId).child("noVote");
+                    currentNodeRef.setValue(newVote).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            String agreedVote = Integer.toString(vote.getYesVote());
+                            String disagreedVote = Integer.toString(vote.getNoVote());
+                            holder.voteCountTV.setText(agreedVote+" people agreed with you and "+disagreedVote+" people disagreed with you!");
+
+                        }
+                    });
                 }
+
+
             }
         });
-        agreedVote = Integer.toString(vote.getYesVote());
 
-        disagreedVote = Integer.toString(vote.getNoVote());
-        holder.voteCountTV.setText(agreedVote+" people agreed with you and "+disagreedVote+" people disagreed with you!");
+
+
 
 
 
@@ -85,7 +130,7 @@ public class CustomAdapertForHome extends RecyclerView.Adapter<CustomAdapertForH
     public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView userNameTV, titleTV, topicTV, voteCountTV, lifetimeTV;
         private EditText commentET;
-        private ImageView submitIV;
+        private Button submitBTN;
         private RadioGroup radioGroup;
 
 
@@ -96,7 +141,7 @@ public class CustomAdapertForHome extends RecyclerView.Adapter<CustomAdapertForH
             topicTV = itemView.findViewById(R.id.topicTVHAUI);
             lifetimeTV = itemView.findViewById(R.id.lifetimeTVHAUI);
             commentET = itemView.findViewById(R.id.commentETHAUI);
-            submitIV = itemView.findViewById(R.id.submitVoteIVHAUI);
+            submitBTN = itemView.findViewById(R.id.submitVoteBTNHAUI);
             radioGroup = itemView.findViewById(R.id.radioGRPHAUI);
             voteCountTV = itemView.findViewById(R.id.voteCountTVHAUI);
         }
